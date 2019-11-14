d2bdc6cc188936ed84ec2d3a677ceeee4cfe8a81
Michael Zhang <mzhang00@stuy.edu>
2019-11-14 05:20:43 -0500

    Finish mandatory tasks

--- statTest.c
+++ statTest.c
@@ -5,11 +5,13 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <time.h>
 
 int main(){
     struct stat sb;
     stat("makefile", &sb);
     printf("File size of makefile: %ld\n", sb.st_size);
-
+    printf("Permissions of makefile: %o\n", sb.st_mode);
+    printf("Time of last access of makefile: %s\n", ctime(&sb.st_atime));
     return 0;
 }
